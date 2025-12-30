5d903c1c42856fe6aac999d69fb29be7bed22d44
MavrickR100 <rahulilk100@gmail.com>
2025-12-30 17:14:12 +0530

    Time: 1 ms (91.07%) | Memory: 42.7 MB (17.71%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int xz=Math.abs(x-z),yz=Math.abs(y-z);
+
+        if(xz==yz) return 0;
+        if(xz<yz){
+            return 1;
+        }else{
+            return 2;
+        }
+
+    }
+}
