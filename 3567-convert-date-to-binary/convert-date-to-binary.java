4b6372d2d34cd83af7476c719f2e0d7544167220
MavrickR100 <rahulilk100@gmail.com>
2025-12-31 15:31:37 +0530

    Time: 13 ms (6.07%) | Memory: 50.2 MB (5.42%) - LeetSync

---
+++ 3567-convert-date-to-binary/convert-date-to-binary.java
@@ -0,0 +1,22 @@
+class Solution {
+    int year, month, day;
+
+    public void convertStringDateToIntVariables(String date) {
+        year = Integer.valueOf(date.substring(0, 4));
+        month = Integer.valueOf(date.substring(5, 7));
+        day = Integer.valueOf(date.substring(8, 10));
+        System.out.print("Day:" + day + " Month:" + month + " Year:" + year);
+    }
+
+    public String convertDateToBinary(String date) {
+        convertStringDateToIntVariables(date);
+
+        String sbr = new String(
+                Integer.toBinaryString(year) + "-" + Integer.toBinaryString(month) + "-" + Integer.toBinaryString(day));
+
+        return sbr;
+
+    }
+}
+
+//amar sunil sakhre
