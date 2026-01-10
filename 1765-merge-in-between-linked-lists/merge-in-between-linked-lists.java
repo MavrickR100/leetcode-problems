d30a20ea437b1a808880368405c0efd76db8fbc9
MavrickR100 <rahulilk100@gmail.com>
2026-01-10 08:14:55 +0530

    Time: 5 ms (3.00%) | Memory: 48.9 MB (95.83%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode prev = list1;
+        ListNode next = list1;
+        ListNode endList2=list2;
+        int node =0;
+        
+        while(prev != null){
+            node++;
+            if(node < a){
+                prev = prev.next;
+            }else{
+                break;
+            }
+        }
+        System.out.println("prev val :"+prev.val);
+        node =0;
+         while(next != null){
+            node++;
+            if(node < b+1){
+                next = next.next;
+            }else{
+                break;
+            }
+        }
+        System.out.println("Next val :"+next.val);
+        // while(prev != null || next != null){
+        //     node++;
+        //     if(node < a){
+        //         prev =prev.next;
+        //     }
+        //     if(node < b){
+        //         next = next.next;
+        //     }
+        //     if(node == b) break;
+        // }
+        while(endList2.next != null){
+            endList2 =endList2.next;
+        }
+
+        prev.next = list2;
+        endList2.next = next.next;
+
+        return list1;
+        
+    }
+}
