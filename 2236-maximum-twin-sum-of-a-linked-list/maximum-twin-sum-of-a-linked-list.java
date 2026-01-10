ab64727e6de2fcece99e47fbf1d03c625c180363
MavrickR100 <rahulilk100@gmail.com>
2026-01-10 10:17:47 +0530

    Time: 19 ms (7.85%) | Memory: 107.2 MB (24.08%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -12,43 +12,25 @@ class Solution {
     public int pairSum(ListNode head) {
         ListNode current1 = head;
         int size=0;
-
+        Stack<Integer> stack = new Stack<>();
         while(current1 != null){
             size ++;
             current1 = current1.next;
         }
         current1 =head;
-
-        for(int i=0;i<size/2-1;i++){
+        for(int i=0;i<size/2;i++){
+            stack.push(current1.val);
             current1=current1.next;
         }
-        ListNode current2 = current1.next;
-        current1.next=null;
-        current1 = head;
-        
-        
-        ListNode prev = null;
-        ListNode next = null;
-
-        while(current2 != null){
-            next = current2.next;
-            current2.next = prev;
-            prev = current2;
-            current2 = next;
-        }
-        current2 = prev;
+        ListNode current2 = current1;
         size=0;
-
-        while(current2 != null && current1 != null){
-        
-        if(current2.val + current1.val > size){
-            size = current2.val + current1.val;
+        while(current2 != null){
+            int t = stack.pop()+current2.val;
+            if( t > size){
+                size = t;
+            }
+            current2 = current2.next;
         }
-        current1=current1.next;
-        current2=current2.next;
-        }
-
-
         return size;
         
     }
