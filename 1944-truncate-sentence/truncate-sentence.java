3929d94befe88669427fd3b2bffc52171838db9b
MavrickR100 <rahulilk100@gmail.com>
2026-02-22 20:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (94.57%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String truncateSentence(String str, int k) {
+
+         
+	    int count =0,i=0;
+	    
+	    for(i=0;i<str.length();i++){
+	        
+	        if(str.charAt(i) == ' '){
+	            count++;
+	        }
+	        if(count == k){
+	            break;
+	        }
+	    }
+	    
+	    String ans = str.substring(0,i); 
+        
+        return ans;
+    }
+}
