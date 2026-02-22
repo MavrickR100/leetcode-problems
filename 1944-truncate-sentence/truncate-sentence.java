c198104bf126f1d14ab533ba8087728ff9877f24
MavrickR100 <rahulilk100@gmail.com>
2026-02-22 20:51:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (46.88%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -1,9 +1,6 @@
 class Solution {
     public String truncateSentence(String str, int k) {
-
-         
 	    int count =0,i=0;
-	    
 	    for(i=0;i<str.length();i++){
 	        
 	        if(str.charAt(i) == ' '){
@@ -13,9 +10,7 @@ class Solution {
 	            break;
 	        }
 	    }
-	    
 	    String ans = str.substring(0,i); 
-        
         return ans;
     }
 }
