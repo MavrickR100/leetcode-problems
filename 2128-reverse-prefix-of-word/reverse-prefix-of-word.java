4c09d8b589101861c95dc17131c56a8d446bf95f
MavrickR100 <rahulilk100@gmail.com>
2026-03-12 21:05:14 +0530

    Time: 1 ms (50.95%) | Memory: 43 MB (67.91%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+
+        int i,z=0;
+
+        for(i=0;i<word.length();i++){
+
+            if(word.charAt(i) == ch){
+            z=i;
+            break;
+            }
+        }
+        
+        String br = new String(new StringBuilder(word.substring(0,z+1)).reverse());
+        br = br+word.substring(z+1,word.length());
+        return br.toString();
+        
+    }
+}
